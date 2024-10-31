767661cc7b523f9fea46a7e34a0094dbb95d3e29
DanielAB95 <danielalvarez.dab@gmail.com>
2024-10-31 22:10:23 +0100

    Nuevos dto de almacenero y paquete

--- src/main/java/persistence/dto/PaqueteDto.java
+++ src/main/java/persistence/dto/PaqueteDto.java
@@ -8,5 +8,16 @@ public class PaqueteDto {
 	public int idPedido;
 	public String paqueteEstado;
 	public Map<ProductoDto, Integer> productos;
+	
+	public String productosToString() {
+		StringBuilder sb = new StringBuilder();
+		for (ProductoDto producto : productos.keySet()) {
+			sb.append("\t");
+			sb.append(producto.nombre + " - ");
+			sb.append(producto.precio + "€ - x ");
+			sb.append(productos.get(producto) + "\n");
+		}
+		return sb.toString();
+	}
 
 }
