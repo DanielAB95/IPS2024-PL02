e0adef110685af5654211fe99b98200df4fe732e
DanielAB95 <danielalvarez.dab@gmail.com>
2024-11-04 03:30:07 +0100

    Ultimo cambio a la vista de wo generadas

--- src/main/java/vista/WorkorderView.java
+++ src/main/java/vista/WorkorderView.java
@@ -7,9 +7,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
@@ -19,8 +17,6 @@ public class WorkorderView extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel lbAlmacenero;
-	private JTextField txAlmacenero;
 	private JScrollPane productoPanel;
 	private JLabel lbProductos;
 	private JTable tbProductos;
@@ -37,9 +33,7 @@ public class WorkorderView extends JDialog {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null); 
-		contentPane.add(getLbAlmacenero());
-		contentPane.add(getTxAlmacenero());
+		contentPane.setLayout(null);
 		contentPane.add(getProductoPanel());
 		contentPane.add(getLbProductos());
 		
@@ -49,30 +43,11 @@ public class WorkorderView extends JDialog {
 		wc.init();
 		
 	}
-	private JLabel getLbAlmacenero() {
-		if (lbAlmacenero == null) {
-			lbAlmacenero = new JLabel("Almacenero responsable:");
-			lbAlmacenero.setFont(new Font("Tahoma", Font.PLAIN, 30));
-			lbAlmacenero.setBounds(10, 11, 444, 39);
-		}
-		return lbAlmacenero;
-	}
-	private JTextField getTxAlmacenero() {
-		if (txAlmacenero == null) {
-			txAlmacenero = new JTextField();
-			txAlmacenero.setHorizontalAlignment(SwingConstants.CENTER);
-			txAlmacenero.setFont(new Font("Tahoma", Font.PLAIN, 30));
-			txAlmacenero.setEditable(false);
-			txAlmacenero.setBounds(10, 61, 444, 39);
-			txAlmacenero.setColumns(10);
-		}
-		return txAlmacenero;
-	}
 	private JScrollPane getProductoPanel() {
 		if (productoPanel == null) {
 			productoPanel = new JScrollPane();
 			productoPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-			productoPanel.setBounds(10, 161, 444, 643);
+			productoPanel.setBounds(10, 61, 444, 743);
 			productoPanel.setViewportView(getTbProductos());
 		}
 		return productoPanel;
@@ -81,7 +56,7 @@ public class WorkorderView extends JDialog {
 		if (lbProductos == null) {
 			lbProductos = new JLabel("Detalles de las Workorders");
 			lbProductos.setFont(new Font("Tahoma", Font.PLAIN, 25));
-			lbProductos.setBounds(10, 111, 444, 39);
+			lbProductos.setBounds(10, 11, 444, 39);
 		}
 		return lbProductos;
 	}
@@ -95,10 +70,6 @@ public class WorkorderView extends JDialog {
 	}
 	
 	//Metodos auxiliares
-	
-	public JTextField getTextAlmacenero() {
-		return this.txAlmacenero;
-	}
 	
 	public JTable getTable() {
 		return this.tbProductos;
